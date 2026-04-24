f6545d52287b1f9304c866a6a7d3992311e2cad6
manisha7343 <manishasharma7343@gmail.com>
2026-04-24 21:51:02 +0530

    added rate limit

--- middleware/rateLimit.js
+++ middleware/rateLimit.js
@@ -1,25 +1,19 @@
 const rateLimit = require('express-rate-limit');
-
-
-//Login Rate Limiter Configuration
+const RedisStore = require('rate-limit-redis');
+const redis = require('../config/redis');
 
 const rateLimiter = rateLimit({
-
-    windowMs: 1 * 60 * 1000,
-    max:5,
-    message:{
-        status: 429,
-        message: "Too many requests! Try after one minute."
-    },
-    standardHeaders: true,
-    legacyHeaders: false,
-
+  windowMs: 1 * 60 * 1000,
+  max: 5,
+  message: {
+    status: 429,
+    message: "Too many requests! Try after one minute."
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+  store: new RedisStore({
+    sendCommand: (...args) => redis.sendCommand(args),
+  }),
 });
 
 module.exports = rateLimiter;
-
-
-
-
-
-
